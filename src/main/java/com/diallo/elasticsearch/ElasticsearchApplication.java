15a33de670cf371a92165dcfbefd8c94937042de
diallo <mamadouadama609@gmail.com>
2024-05-03 23:16:55 -0500

    config file to configure elasticsearch bean and setup the localhost:9200 url

--- src/main/java/com/diallo/elasticsearch/ElasticsearchApplication.java
+++ src/main/java/com/diallo/elasticsearch/ElasticsearchApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElasticsearchApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Test");
+
 		SpringApplication.run(ElasticsearchApplication.class, args);
 	}
 
---
+++ src/main/java/com/diallo/elasticsearch/config/MyClientConfig.java
@@ -0,0 +1,19 @@
+package com.diallo.elasticsearch.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class MyClientConfig extends ElasticsearchConfiguration {
+
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration
+                .builder()
+                .connectedTo("localhost:9200")
+                .build();
+    }
+}
